84ec412e487200c377e4d63be10bfedfc089dc74
3XTR4OS <84981999+3XTR4OS@users.noreply.github.com>
2022-04-04 08:37:10 +1000

    Update main.py

--- main.py
+++ main.py
@@ -8,23 +8,23 @@ def check_for_none(param):
     return param
 
 
-def get_parameters(row):  # v means vacancy
-    v_name = check_for_none(row['name'])
-    v_url = check_for_none(row['alternate_url'])
+def get_vacancy_parameters(row):  # v means vacancy
+    name = check_for_none(row['name'])
+    url = check_for_none(row['alternate_url'])
 
     if row['salary'] is not None:
-        v_from = check_for_none(row['salary']['from'])
-        v_to = check_for_none(row['salary']['to'])
+        salary_from = check_for_none(row['salary']['from'])
+        salary_to = check_for_none(row['salary']['to'])
     else:
-        v_from = 0
-        v_to = 0
+        salary_from = 0
+        salary_to = 0
 
     if row['schedule']['id'] == 'remote':
-        v_remote = 'Возможна удалённая работа'
+        remote = 'Возможна удалённая работа'
     else:
-        v_remote = 'Удалённая работа не возможна'
+        remote = 'Удалённая работа не возможна'
 
-    return v_name, v_url, [v_from, v_to], v_remote
+    return name, url, [salary_from, salary_to], remote
 
 
 URL = 'https://api.hh.ru/vacancies'
@@ -46,7 +46,7 @@ def find_vacancy(pages=100):
 
     request = requests.get(URL, params=par).json()
     for item in request['items']:
-        print(*get_parameters(item))
+        print(*get_vacancy_parameters(item))
 
 
 if __name__ == '__main__':
